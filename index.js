9bd83f20706ad6e6aefdd092978582150fc9b62b
tunagezgin <tunagezgin04@gmail.com>
2025-09-18 14:48:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Merhaba, Private EC2!</h1>');
+  res.send('<h1>Merhaba, Private EC2!-değişiklik-</h1>');
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
